87378652c4a80bbb23d33b5efa28df52ed7d1010
Yuvraj Singh <yuvraj.singh@xarterian.com>
2025-01-14 15:48:39 +0530

    Added browser router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-
-import './App.css';
-import Home from './pages/Home';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home'; // Your homepage component
+import MovieDetails from './pages/MovieDetails'; // Your movie details component
 
 function App() {
   return (
-    <>
-    <Home/>
-    <div className='text-red-500'>Jai Shree Ram</div>
-    
-    </>
-     
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie/:id" element={<MovieDetails />} /> {/* Dynamic route */}
+      </Routes>
+    </Router>
   );
 }
 
