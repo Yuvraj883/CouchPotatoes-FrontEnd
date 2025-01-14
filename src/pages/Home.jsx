7b4845b63a8636878d8a418cafdadc3641e6df2f
Yuvraj Singh <yuvraj.singh@xarterian.com>
2025-01-14 12:16:52 +0530

    Created Home section

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../component/Header'
+import HeroSection from '../component/HeroSection'
+
+export default function Home () {
+  return (
+    <>
+    <Header></Header>
+    <HeroSection></HeroSection>
+    </>
+  )
+}
