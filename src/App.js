5029a75b19e6cbd217e71b2a015b68399717ffa4
Yuvraj Singh <yuvraj.singh@xarterian.com>
2025-01-15 14:11:39 +0530

    Added top-rated movies route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import MovieDetails from './pages/MovieDetails'; // Your movie details component
 import Header from './component/Header';
 import Footer from './component/Footer';
 import AboutPage from './pages/About';
+import TopRatedMovies from './pages/TopRated';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/movie/:id" element={<MovieDetails />} /> {/* Dynamic route */}
         <Route path='/about' element={<AboutPage/>}/>
+        <Route path='/top-rated' element={<TopRatedMovies/>}/>
       </Routes>
       <Footer/>
     </Router>
