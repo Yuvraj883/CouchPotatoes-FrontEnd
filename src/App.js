a68915d6f35a26824df653748f335861979fe026
Yuvraj Singh <yuvraj.singh@xarterian.com>
2025-01-15 13:53:36 +0530

    Added route to app.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/Home'; // Your homepage component
 import MovieDetails from './pages/MovieDetails'; // Your movie details component
 import Header from './component/Header';
 import Footer from './component/Footer';
+import AboutPage from './pages/About';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/movie/:id" element={<MovieDetails />} /> {/* Dynamic route */}
+        <Route path='/about' element={<AboutPage/>}/>
       </Routes>
       <Footer/>
     </Router>
