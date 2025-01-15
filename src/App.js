f2253a6ee22004f1f1fd74d0bd28136c8ff8f86f
Yuvraj Singh <yuvraj.singh@xarterian.com>
2025-01-15 11:38:29 +0530

    Added Header and Footer as universal components

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home'; // Your homepage component
 import MovieDetails from './pages/MovieDetails'; // Your movie details component
+import Header from './component/Header';
+import Footer from './component/Footer';
 
 function App() {
   return (
     <Router>
+      <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/movie/:id" element={<MovieDetails />} /> {/* Dynamic route */}
       </Routes>
+      <Footer/>
     </Router>
   );
 }
