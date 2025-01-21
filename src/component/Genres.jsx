8bc7e603c974b310039a9f8c2e55f54542aedacc
Yuvraj Singh <yuvraj.singh@xarterian.com>
2025-01-21 10:45:31 +0530

    Updated style

--- src/component/Genres.jsx
+++ src/component/Genres.jsx
@@ -41,7 +41,7 @@ const GenreList = ({ onGenreSelect, selectedGenres }) => {
           <li
             key={index}
             className={`text-gray-700 px-3 py-1 h-8 rounded-l-full rounded-r-full mx-2 cursor-pointer hover:bg-blue-500 hover:text-white
-              ${selectedGenres.includes(genre) ? 'bg-blue-500 text-white' : 'bg-white'}`}
+              ${selectedGenres?.includes(genre) ? 'bg-blue-500 text-white' : 'bg-white'}`}
             onClick={() => handleGenreClick(genre)} // Call onGenreSelect when a genre is clicked
           >
             {genre}
